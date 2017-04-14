bf40b141babd86a054c261989e5fdcad09417e44
Mercerenies <sdmayolo42@students.tntech.edu>
2017-04-13 19:02:50 -0500

    Convenience script

---
+++ etc/unused.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use 5.010;
+
+use Data::Dumper;
+
+my $fh;
+my @alpha;
+
+open($fh, '<', './Shiny/Case.hs');
+for (<$fh>) {
+    s/\r\n/\n/;
+    chomp;
+    @alpha = split //, $1 if /^lowerCase = "([^"]*)"$/
+}
+close($fh);
+
+my %alpha;
+
+@alpha{@alpha} = ();
+#@alpha{ map { my $x = $_; map $x . $_, @alpha } @alpha } = ();
+
+open($fh, '<', './Shiny/Standard.hs');
+for (<$fh>) {
+    $alpha{$1}++ if /"([^"]{0,2})"/;
+}
+close($fh);
+
+for (keys %alpha) {
+    print "$_\n" unless $alpha{$_};
+}
